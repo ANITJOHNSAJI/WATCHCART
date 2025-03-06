18525df48acb8eab75d7e2d36e82b801cd435423
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-03-06 13:34:31 +0530

    6/3/25

--- epro/eapp/urls.py
+++ epro/eapp/urls.py
@@ -3,5 +3,12 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path('', views.userlogin,name='userlogin'),
+    path('signup',views.usersignup,name='usersignup'),
+    path('index',views.index,name='index'),
+    path('forgotpassword',views.getusername,name='forgotpassword'),
+    path('verifyotp',views.verifyotp,name='verifyotp'),
+    path('passwordreset',views.passwordreset,name='passwordreset'),
+    path('logout',views.logoutuser,name="logout"),
     
 ]
